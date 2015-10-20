4261c600559666a01eae657c496bc498a9579ea0
Mayank Kale <kalemayank29@gmail.com>
2015-10-20 17:03:46 -0500

    d

--- stocklist/views.py
+++ stocklist/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from stocklist.models import Stock
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1> Welcome to Equity </h1>")
+    stock_list = Stock.objects.all()
+    output = ', '.join([p.tickr for p in stock_list])
+    return HttpResponse(output)
+
+    #return HttpResponse("<h1> Welcome to Equity </h1>")
